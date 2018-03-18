f45a6c11ee5faff463f91721669c3fb757068d27
Marcos Schead <wssmarcos@gmail.com>
2018-03-18 15:07:29 -0300

    Making device login endpoint with jwt

--- server/middleware.js
+++ server/middleware.js
@@ -1,6 +1,7 @@
 const { user } = require('./user');
+const { device } = require('./device');
 
-var authenticate = (req, res, next) => {
+var authenticateUser = (req, res, next) => {
   var token = req.header('x-auth');
 
   if (user.tokenData.token === token) {
@@ -12,4 +13,16 @@ var authenticate = (req, res, next) => {
   }
 };
 
-module.exports = { authenticate };
+var authenticateDevice = (req, res, next) => {
+  var token = req.header('x-auth');
+
+  if (device.findDeviceByToken(token)) {
+    req.device = device;
+    req.token = token;
+    next();
+  } else {
+    res.status(401).send();
+  }
+}
+
+module.exports = { authenticateUser, authenticateDevice };
