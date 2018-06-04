50bd2a9f59b0818cf2626beb2a427cf23447623b
Marcos Schead <wssmarcos@gmail.com>
2018-06-04 20:42:33 -0300

    Device list with adapter; fix in the next NDC generation

--- app/AppService/src/main/java/com/schead/tcc/smartphoneauthenticator/DeviceListActivity.java
+++ app/AppService/src/main/java/com/schead/tcc/smartphoneauthenticator/DeviceListActivity.java
@@ -3,6 +3,10 @@ package com.schead.tcc.smartphoneauthenticator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DeviceListActivity extends AppCompatActivity {
 
@@ -12,6 +16,10 @@ public class DeviceListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_device_list);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        DeviceArrayAdapter adapter = new DeviceArrayAdapter(this, getModel());
+        ListView listView = (ListView) findViewById(R.id.list_device);
+        listView.setAdapter(adapter);
     }
 
     @Override
@@ -22,5 +30,19 @@ public class DeviceListActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private List<Device> getModel() {
+        List<Device> list = new ArrayList<Device>();
+        list.add(get("device_001"));
+        list.add(get("device_002"));
+        list.add(get("device_003"));
+        // Initially select one of the items
+        list.get(1).setSelected(true);
+        return list;
+    }
+
+    private Device get(String s) {
+        return new Device(s);
     }
 }
